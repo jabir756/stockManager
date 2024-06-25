e864713f1b0ea909357a4abd7c3b1c7e6ae985d1
jabir756 <jabirkagone6@gmail>
2024-06-25 16:35:40 -0400

    move all files on folder root

--- indexScript.js
+++ indexScript.js
@@ -1,15 +1,15 @@
 $(document).ready(()=>{
 
     $(document).on('click','.list-products-button',()=>{
-        window.location.href = '../html/listProducts.html';      
+        window.location.href = 'listProducts.html';      
     });
 
     $(document).on('click', '.remove-products-button', () => {
-        window.location.href = '../html/removeProducts.html';
+        window.location.href = 'removeProducts.html';
     });
 
     $(document).on('click','.add-products-button', () => {       
-        window.location.href = '../html/addProducts.html';
+        window.location.href = 'addProducts.html';
     });
 
 
